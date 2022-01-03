72298e2efd756cdcbb494648fb3b7ad0f9f61c6c
i5heu <i5heu@i5h.eu>
2022-01-03 12:03:24 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# socks5-rss-to-twitter
+# socks5-rss-to-mastodon
+Will post a post from a RSS feed to Mastodon.
+If the RSS feed is hosted on TOR then the service qr2tor.net will be used.
+
+`node main.js -r '[rss-link]' -a '[mastodon access token]' -p '[ip of proxy]:9050'`
---
+++ main.js
@@ -0,0 +1,135 @@
+const url = require('url');
+const http = require('http');
+const https = require('https');
+var FormData = require('form-data');
+const fs = require('fs');
+const SocksProxyAgent = require('socks-proxy-agent');
+let Parser = require('rss-parser');
+let parser = new Parser();
+const commandLineArgs = require('command-line-args')
+
+
+const optionDefinitions = [
+    { name: 'rss-url', alias: 'r', type: String },
+    { name: 'auth', alias: 'a', type: String },
+    { name: 'proxy', alias: 'p', type: String },
+];
+
+
+const usrOptions = commandLineArgs(optionDefinitions)
+
+// Use the SOCKS_PROXY env var if using a custom bind address or port for your TOR proxy:
+const proxy = process.env.SOCKS_PROXY || 'socks5h://' + usrOptions.proxy;
+console.log('Using proxy server %j', proxy);
+// The default HTTP endpoint here is DuckDuckGo's v3 onion address:
+const endpoint = usrOptions['rss-url'];
+console.log('Attempting to GET %j', endpoint);
+// Prepare options for the http/s module by parsing the endpoint URL:
+let options = url.parse(endpoint);
+const agent = new SocksProxyAgent(proxy);
+// Here we pass the socks proxy agent to the http/s module:
+options.agent = agent;
+// Depending on the endpoint's protocol, we use http or https module:
+const httpOrHttps = options.protocol === 'https:' ? https : http;
+// Make an HTTP GET request:
+
+httpOrHttps.get(options, res => {
+    var body = '';
+
+    res.on('data', function (chunk) {
+        body += chunk;
+    });
+
+    res.on('end', async () => {
+        let feed = await parser.parseString(body);
+
+        for (const item of feed.items) {
+            await handleItem(item)
+        }
+
+        console.log("Done");
+    });
+});
+
+async function handleItem(item) {
+    if (isAlreadyPosted(item)) return;
+
+    if (200 == await postToMastodon(item))
+        writePostInFile(item);
+}
+
+async function postToMastodon(item) {
+
+    const formData = new FormData();
+
+    formData.append("status", "Check out this article: " + "https://qr2tor.net/" + encodeURIComponent(item.link));
+
+    const optionsMastodon = {
+        hostname: 'mastodon.social',
+        port: 443,
+        path: '/api/v1/statuses',
+        method: 'POST',
+        agent: new SocksProxyAgent(proxy),
+        headers: Object.assign(
+            formData.getHeaders(),
+            {
+                'Authorization': 'Bearer ' + usrOptions.auth,
+            })
+    };
+
+    console.log("POST: " + item.title);
+    const req = https.request(optionsMastodon, res => { });
+
+    formData.pipe(req);
+    req.end();
+
+    return new Promise((resolve, reject) => {
+        req.on('response', function (res) {
+            console.log(res.statusCode);
+            if (res.statusCode == 200) {
+                resolve(res.statusCode);
+            } else {
+                reject(res.statusCode);
+            }
+        });
+    });
+}
+
+function isAlreadyPosted(item) {
+    var data = fs.readFileSync('postedUrls.txt', 'utf8').toString();
+    return data.includes(item.link);
+}
+
+function writePostInFile(item) {
+    fs.writeFileSync('postedUrls.txt', item.link + "\n", { flag: 'a+' });
+}
+
+/**
+ * Estimate the content length of (multipart/form-data) encoded form data
+ * object (sent in HTTP POST requests).
+ * We do not know if you can get the actual content length.
+ * Hence, it is estimated by this function.
+ * As soon as {@link https://stackoverflow.com/q/62281752/1065654 this}
+ * question is answered (correctly), the correct calculation should be used.
+ *
+ * @param formData
+ */
+function estimateContentLength(formData) {
+    // Seems to be 44 in WebKit browsers (e.g. Chrome, Safari, etc.),
+    // but varies at least in Firefox.
+    const baseLength = 50; // estimated max value
+    // Seems to be 87 in WebKit browsers (e.g. Chrome, Safari, etc.),
+    // but varies at least in Firefox.
+    const separatorLength = 115; // estimated max value
+    let length = baseLength;
+    const entries = formData.entries();
+    for (const [key, value] of entries) {
+        length += key.length + separatorLength;
+        if (typeof value === 'object') {
+            length += value.size;
+        } else {
+            length += String(value).length;
+        }
+    }
+    return length;
+}
